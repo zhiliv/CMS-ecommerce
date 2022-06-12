e27b0ab47e8dc349f289d27ccb830433b460f1b0
Ivan Zhilnikov <izhilnikov@bars.group>
2022-06-12 23:10:31 +0300

    fix

--- components/app/hr/hr.vue
+++ components/app/hr/hr.vue
@@ -11,7 +11,7 @@ export default {
    * @property {String} classes - Входные классы
    * @property {String} styles - Входные стили
    */
-  prop: {
+  props: {
     classes: {
       type: String,
       default: '',
