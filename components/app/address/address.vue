d35d43225af4c736533b6f35585fe931104a1dd0
Ivan Zhilnikov <izhilnikov@bars.group>
2022-06-12 23:23:09 +0300

    Добавление компонента "Адрес" app-address, тестирования и результата тестирования

---
+++ components/app/address/address.vue
@@ -0,0 +1,34 @@
+<template>
+  <address :style="styles" :class="classes">
+    <slot></slot>
+  </address>
+</template>
+
+<script>
+export default {
+  /*
+   * Входящие свойства
+   * @typedef {Object} props
+   * @property {String} classes - Входные классы
+   * @property {String} styles - Входные стили
+   */
+  props: {
+    classes: {
+      type: String,
+      default: '',
+    },
+    styles: {
+      type: String,
+      default: '',
+    },
+  },
+}
+</script>
+
+<style>
+address {
+  margin-bottom: 1rem;
+  font-style: normal;
+  line-height: inherit;
+}
+</style>
