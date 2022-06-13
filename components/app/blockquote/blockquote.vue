621c140062029f0b02b0753ed2e61be4b98b9115
Ivan Zhilnikov <izhilnikov@bars.group>
2022-06-13 19:47:15 +0300

    Добавление компоненета app-blockquote

---
+++ components/app/blockquote/blockquote.vue
@@ -0,0 +1,32 @@
+<template>
+  <blockquote :class="classes" :style="style">
+    <slot></slot>
+  </blockquote>
+</template>
+
+<script>
+export default {
+  /*
+   * Входящие свойства
+   * @typedef {Object} props
+   * @property {String} classes - Входные классы
+   * @property {String} styles - Входные стили
+   */
+  props: {
+    classes: {
+      type: String,
+      default: '',
+    },
+    styles: {
+      type: String,
+      default: '',
+    },
+  },
+}
+</script>
+
+<style>
+blockquote {
+  margin: 0 0 1rem;
+}
+</style>
---
+++ test/app-blockquote.spec.js
@@ -0,0 +1,24 @@
+import { shallowMount } from '@vue/test-utils'
+import ADDRESS from '@/components/app/blockquote/blockquote.vue'
+
+describe('app-blockquote.js', () => {
+  let cmp
+
+  beforeEach(() => {
+    cmp = shallowMount(ADDRESS, {
+      // Создать поверхностный экземпляр компонента
+      propsData: {
+        classes: 'simple simple-xl',
+        styles: 'background: white',
+        id: 'indent'
+      },
+
+      slots:{
+        default: 'Содержимое'
+      }
+    })
+  })
+  it('Содержимое html для app-blockquote ', () => {
+    expect(cmp.element).toMatchSnapshot()
+  })
+})
