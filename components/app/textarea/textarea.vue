953fcd715ac137b690f22249d1a8be36e6760682
Ivan Zhilnikov <izhilnikov@bars.group>
2022-06-23 22:01:11 +0300

    Добавление компонента app-textarea

---
+++ components/app/textarea/textarea.vue
@@ -0,0 +1,74 @@
+<template>
+  <textarea
+    :class="[outClass, classes]"
+    :style="styles"
+    v-bind="$attrs"
+    @dblclick="$emit('dblclick', $event)"
+    @click="$emit('click', $event)"
+    @change="$emit('change', $event)"
+    @blur="$emit('blur', $event)"
+    @focus="$emit('focus', $event)"
+    @keydown="$emit('keydown', $event)"
+    @keypress="$emit('keypress', $event)"
+    @keyup="$emit('keyup', $event)"
+    @load="$emit('load', $event)"
+    @mousedown="$emit('mousedown', $event)"
+    @mousemove="$emit('mousemove', $event)"
+    @mouseout="$emit('mouseout', $event)"
+    @mouseover="$emit('mouseover', $event)"
+    @mouseup="$emit('mouseup', $event)"
+    @reset="$emit('reset', $event)"
+    @select="$emit('select', $event)"
+    @submit="$emit('submit', $event)"
+    @unload="$emit('unload', $event)"
+  >
+    <slot></slot>
+  </textarea>
+</template>
+
+<script>
+export default {
+  /*
+   * Входящие свойства
+   * @typedef {Object} props
+   * @property {String} classes - Входные классы
+   * @property {String} styles - Входные стили
+   */
+  props: {
+    classes: {
+      type: String,
+      default: '',
+    },
+    styles: {
+      type: String,
+      default: '',
+    }
+  },
+  data() {
+    return {
+      outClass: null, // классы, сформированные аттрибутами
+    }
+  },
+
+}
+</script>
+
+<style>
+textarea {
+  margin: 0;
+  font-family: inherit;
+  font-size: inherit;
+  line-height: inherit;
+  resize: vertical;
+}
+
+textarea.form-control {
+  min-height: calc(1.5em + 0.75rem + 2px);
+}
+textarea.form-control-sm {
+  min-height: calc(1.5em + 0.5rem + 2px);
+}
+textarea.form-control-lg {
+  min-height: calc(1.5em + 1rem + 2px);
+}
+</style>
