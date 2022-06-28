e24f1d48cea8a62e6ee78f2d805611d5f33dade0
Ivan Zhilnikov <izhilnikov@bars.group>
2022-06-28 22:48:19 +0300

    Добавление компонента ul-list

---
+++ test/app-ul.spec.js
@@ -0,0 +1,25 @@
+import { shallowMount } from '@vue/test-utils'
+import OL from '@/components/app/ol-ul-dl/ol-ul-dl.vue'
+
+describe('app-ol-ul.js', () => {
+  let cmp
+
+  beforeEach(() => {
+    cmp = shallowMount(OL, {
+      // Создать поверхностный экземпляр компонента
+      propsData: {
+        classes: 'simple simple-xl',
+        styles: 'background: white',
+        id: 'indent',
+        type: 'ul'
+      },
+
+      slots:{
+        default: '<li>Тест</li>'
+      }
+    })
+  })
+  it('Содержимое html для app-ol-ul ', () => {
+    expect(cmp.element).toMatchSnapshot()
+  })
+})
